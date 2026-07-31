712a2c28006c1170c5ac326b17a215c56cf7c2fb
linlang654 <255297555+linlang654@users.noreply.github.com>
2026-07-31 14:43:03 +0800

    feat: 批量生成出库贴纸打印文件

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "shitong-cloud-v25";
+const CACHE_NAME = "shitong-cloud-v26";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=25",
-  "./app.js?v=25",
+  "./styles.css?v=26",
+  "./app.js?v=26",
   "./manifest.webmanifest",
   "./courier.html",
   "./factory.html",
